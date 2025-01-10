dab18c087e9d3258aabc8f54ad0aa51675e5e3cf
zigg17 <jacob.ziegler@colorado.edu>
2025-01-09 19:57:36 -0700

    update: removal of complexity

--- misc/misc-data-aug/permissions_updater.sh
+++
@@ -1,7 +0,0 @@
-DIRECTORY="/Users/jakeziegler/Desktop/PROJECTS/INSTA/data/like-comment/Screenshots1"
-
-find "$DIRECTORY" -type f | while read -r FILE; do
-    sudo chown jakeziegler:staff "$FILE"
-    sudo chmod u+w "$FILE"
-    echo "Updated: $FILE"
-done
